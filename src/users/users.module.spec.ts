351de3b97c3a791306718fc91fc8963fb2a9de46
Vladimir Kromm <vkromm@gmail.com>
2025-04-08 11:15:45 +0200

    пример интеграционного теста

--- src/users/users.module.spec.ts
+++ src/users/users.module.spec.ts
@@ -1,22 +1,52 @@
 import { DuplicateError, UsersService } from "./users.service";
+import * as request from 'supertest';
+
 import { Test } from "@nestjs/testing";
 import { UsersController } from "./users.controller";
+import { DatabaseModule } from "../database/database.module";
+import { Sequelize } from "sequelize-typescript";
+import { User } from "./models/user.model";
+import { INestApplication } from "@nestjs/common";
+import { UsersModule } from "./users.module";
 
 describe('CRUD in UserService', () => {
+  let app: INestApplication;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        UsersController,
-      ],
-    //   мокаем не код, а базу данных целиком (ин-мемори база данных)
-    }).compile();
+      imports: [UsersModule]
+    })
+      .overrideProvider("Sequelize")
+      .useFactory({
+        // inject: [],
+        factory: async () => {
+              const sequelize = new Sequelize({
+                storage: ':memory:',
+                dialect: 'sqlite',
+              });
+              sequelize.addModels([User]);
+              await sequelize.sync();
+              return sequelize;
+        }
+      })
+      .compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
 
   });
 
-бефо алл - создаем 2 пользователя
+  beforeAll(async ()=>{
+    await User.create({login: "123", password: "123", firstName: "123"})
+    // создаем 2 пользователя
+  })
 
-  it('гет запрос на 1 дает пользоывателя', async () => {
+  it(`/GET userId = 1`, () => {
+    return request(app.getHttpServer())
+      .get('/users/1')
+      .expect(200)
+      .expect({
+        data: "123",
+      });
   });
 
   it('гет запрос на /дает список из 2 пользоывателей', async () => {
